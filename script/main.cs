97b5763edcb74226e4787d43d3d1b8e6435a1718
Revg0d <HELLSTORM696969@GMAIL.COM>
2025-09-02 16:57:20 +0530

    Add files via upload

---
+++ script/main.cs
@@ -0,0 +1,16 @@
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class main : MonoBehaviour
+{
+    public void playgame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void quitgame()
+    {
+        Application.Quit();
+    }
+}
